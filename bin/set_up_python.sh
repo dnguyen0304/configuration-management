38ca865c0f24da12954dc614e119af091f859e9a
Duy Nguyen <dnguyen0304@gmail.com>
2017-02-28 07:18:54 -0500

    N/A: Merged from Google Drive

--- bin/set_up_python.sh
+++ bin/set_up_python.sh
@@ -2,14 +2,13 @@
 
 # Requirements
 # ------------
+# Base
 
 python_version=$1
 
-sudo apt-get -y update
+sudo apt-get update -y
 
-sudo apt-get -y --no-install-recommends install wget
-
-sudo apt-get -y --no-install-recommends install build-essential libbz2-dev libgdbm-dev libncursesw5-dev libreadline-dev libsqlite3-dev libssl-dev libz-dev
+sudo apt-get install -y --no-install-recommends build-essential libbz2-dev libgdbm-dev libncursesw5-dev libreadline-dev libsqlite3-dev libssl-dev libz-dev
 # Should the following line be added here?
 # rm -r /var/lib/apt/lists
 wget https://www.python.org/ftp/python/$python_version/Python-$python_version.tar.xz
@@ -24,7 +23,7 @@ rm Python-$python_version.tar.xz
 sudo rm -r Python-$python_version
 wget https://bootstrap.pypa.io/get-pip.py
 sudo python get-pip.py
-mkdir --parents ~/.config/pip
+mkdir --parents ~/.config/pip/
 if ! grep --quiet "\[list\]" ~/.config/pip/pip.conf; then
     echo -e "[list]\nformat = columns" >> ~/.config/pip/pip.conf
 fi
