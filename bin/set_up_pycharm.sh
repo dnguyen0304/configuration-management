b5f38385cdf537a4e51ffc80141ab20d75df34a4
Duy Nguyen <dnguyen0304@gmail.com>
2017-02-28 11:35:16 -0500

    N/A: Changed to use variables

--- bin/set_up_pycharm.sh
+++ bin/set_up_pycharm.sh
@@ -2,14 +2,22 @@
 
 # Requirements
 # ------------
+# Base
+#
+# Reminders
+# ---------
+# Change the configuration file.
 
-pycharm_version=2016.3.2
+file_name="pycharm-community-2016.3.2"
+compressed_file_name="$file_name.tar.gz"
+directory_path="/usr/local/bin/$file_name"
 
 set -e
 
-sudo apt-get -y --no-install-recommends install tar wget
+mkdir $directory_path
 
-wget https://download.jetbrains.com/python/pycharm-community-$pycharm_version.tar.gz
-tar -xf pycharm-community-$pycharm_version.tar.gz
-sudo mv pycharm-community-$pycharm_version /usr/local/bin/
-rm pycharm-community-$pycharm_version.tar.gz
+wget https://download.jetbrains.com/python/$compressed_file_name
+tar -xf $compressed_file_name --directory $directory_path --strip-components 1
+mv $compressed_file_name $directory_path
+
+chown --recursive ubuntu:ubuntu $directory_path
