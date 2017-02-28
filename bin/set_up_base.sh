e17159254e1476e067359550a46871500bde1a62
Duy Nguyen <dnguyen0304@gmail.com>
2017-02-28 07:17:58 -0500

    N/A: Added initial file

---
+++ bin/set_up_base.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+# Requirements
+# ------------
+
+echo 'export HISTTIMEFORMAT="%Y-%m-%d %T "' >> ~/.bashrc
+source ~/.bashrc
+
+## If there is a prompt asking about configuring GRUB, select "/dev/sda".
+apt-get update -y && apt-get dist-upgrade -y && apt-get autoremove -y
+
+apt-get install -y --no-install-recommends vim wget
