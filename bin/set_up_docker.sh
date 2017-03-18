7557f73a25617ad084d1f8b0b0587e7a62794235
ubuntu <dnguyen0304@gmail.com>
2017-03-18 16:29:45 -0400

    N/A: Removed unnecessary sudo
    Changed code styling

--- bin/set_up_docker.sh
+++ bin/set_up_docker.sh
@@ -2,22 +2,22 @@
 
 # Requirements
 # ------------
+# Base
 
 set -e
 
-sudo apt-get -y update
-
-sudo apt-get -y --no-install-recommends install wget
-
-sudo apt-get -y --no-install-recommends install linux-image-extra-$(uname -r) linux-image-extra-virtual
-sudo apt-get -y --no-install-recommends install apt-transport-https ca-certificates software-properties-common
-wget -q -O - https://yum.dockerproject.org/gpg | sudo apt-key add -
 distribution_name=$(lsb_release -cs)
 if [ $distribution_name = "freya" ]; then
     distribution_name="trusty"
 fi
-sudo add-apt-repository "deb https://apt.dockerproject.org/repo/ ubuntu-$distribution_name main"
 
-sudo apt-get -y update
+apt-get update -y
 
-sudo apt-get -y --no-install-recommends install docker-engine
+apt-get install -y --no-install-recommends linux-image-extra-$(uname -r) linux-image-extra-virtual
+apt-get install -y --no-install-recommends apt-transport-https ca-certificates software-properties-common
+wget -q -O - https://yum.dockerproject.org/gpg | apt-key add -
+add-apt-repository "deb https://apt.dockerproject.org/repo/ ubuntu-$distribution_name main"
+
+apt-get update -y
+
+apt-get install -y --no-install-recommends docker-engine
